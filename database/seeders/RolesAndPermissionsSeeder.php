8f7801394f7d87c0eb05177e09f988bd7adf9e70
Francesco <hlevel648gmail.com>
2026-05-21 14:21:45 +0200

    feat: add roles and permissions with Spatie

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cache dei permessi
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Crea permessi
+        Permission::create(['name' => 'manage teams']);
+        Permission::create(['name' => 'manage projects']);
+        Permission::create(['name' => 'manage tasks']);
+        Permission::create(['name' => 'manage invoices']);
+        Permission::create(['name' => 'view projects']);
+        Permission::create(['name' => 'view invoices']);
+
+        // Crea ruoli e assegna permessi
+        $admin = Role::create(['name' => 'admin']);
+        $admin->givePermissionTo(Permission::all());
+
+        $manager = Role::create(['name' => 'manager']);
+        $manager->givePermissionTo([
+            'manage projects',
+            'manage tasks',
+            'view invoices',
+        ]);
+
+        $client = Role::create(['name' => 'client']);
+        $client->givePermissionTo([
+            'view projects',
+            'view invoices',
+        ]);
+    
+    }
+}
